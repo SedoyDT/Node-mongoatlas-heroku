427954f696bdecbc148d120a49660e4fca864a31
SedoyDT <47536998+SedoyDT@users.noreply.github.com>
2019-09-29 17:33:28 +0300

    Delete keys.dev.js

--- keys/keys.dev.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://Anatoliy:i5w3mvc8Gg6BqzSB@cluster0-zhkqm.mongodb.net/test?retryWrites=true&w=majority' ,
-  SESSION_SECRET: 'some secret value ',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM:"nodejs-courses@wfm.ru ",
-  BASE_URL: "http://localhost:3000"
-} 
